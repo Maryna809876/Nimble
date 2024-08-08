fc20d6b42980add09396ad1b655dfa2d2d555fb5
Марина Арапова <marinchik904@gmail.com>
2024-08-09 01:19:09 +0300

    add

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,20 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-    plugins: [react(), svgr()],
-    base: '/Nimble/',
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://cors-anywhere.herokuapp.com/https://live.devnimble.com',
-                changeOrigin: true,
-                secure: false,
-                headers: {
-                    'Origin': 'http://localhost:5175',
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [react(), svgr()],
+        // base: mode === 'production' ? '/Nimble/' : '/',
+        base: "/",
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'https://cors-anywhere.herokuapp.com/https://live.devnimble.com',
+                    changeOrigin: true,
+                    secure: false,
+                    headers: {
+                        'Origin': 'http://localhost:5175',
+                        'X-Requested-With': 'XMLHttpRequest'
+                    }
+                },
             },
         },
-    },
+    };
 });
